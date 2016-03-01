0bc0a8546b95c25b0751f8721acb30c45d89e303
Michele Belluco <bellucomichele@gmail.com>
2016-03-01 23:10:40 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+
+
+module.exports = {
+	"BaseView":           require("./lib/BaseView"),
+	"PageView":           require("./lib/components/PageView"),
+	"ImageView":          require("./lib/components/ImageView"),
+	"RateView":           require("./lib/components/RateView"),
+	// Component - Carousel
+	"CarouselView":       require("./lib/components/carousel/CarouselView"),
+	"BreadcrumbView":     require("./lib/components/carousel/BreadcrumbView"),
+	// Component - Dialogs
+	"ImageViewModalView": require("./lib/components/dialogs/ImageViewModalView"),
+	"LookupListView":     require("./lib/components/dialogs/LookupListView"),
+	"LookupModalView":    require("./lib/components/dialogs/LookupModalView"),
+	"ModalView":          require("./lib/components/dialogs/ModalView"),
+	// Component - Forms
+	// "FormView":           require("./lib/components/forms/FormView"),
+	// "TextField":          require("./lib/components/forms/TextField"),
+	// "CheckboxField":      require("./lib/components/forms/CheckboxField"),
+	// "CreditCardField":    require("./lib/components/forms/CreditCardField"),
+	// "SearchFilterView":   require("./lib/components/forms/SearchFilterView"),
+	// "SelectField":        require("./lib/components/forms/SelectField"),
+	// "SwitchView":         require("./lib/components/forms/SwitchView"),
+	// // Component - List view
+	// "ListView":           require("./lib/components/listviews/ListView"),
+	// "FixOverflowScroll":  require("./lib/components/listviews/FixOverflowScroll"),
+	// "ListItemView":       require("./lib/components/listviews/ListItemView"),
+	// "FilteredListView":   require("./lib/components/listviews/FilteredListView"),
+	// "GroupListItemView":  require("./lib/components/listviews/GroupListItemView"),
+	// "GroupListView":      require("./lib/components/listviews/GroupListView"),
+	// "HorizontalListView": require("./lib/components/listviews/HorizontalListView"),
+	// "InfiniteListView":   require("./lib/components/listviews/InfiniteListView"),
+	// "OptionListItemView": require("./lib/components/listviews/OptionListItemView"),
+	// "OptionListView":     require("./lib/components/listviews/OptionListView"),
+	// "PaginatedListView":  require("./lib/components/listviews/PaginatedListView"),
+	// "RemoteListView":     require("./lib/components/listviews/RemoteListView"),
+	// "SnapListView":       require("./lib/components/listviews/SnapListView"),
+	// // Component - Loadings
+	// "LoadingBarView":     require("./lib/components/loadings/LoadingBarView"),
+	// "LoadingScreenView":  require("./lib/components/loadings/LoadingScreenView"),
+	// // Component - Maps
+	// "FilteredMapView":    require("./lib/components/maps/FilteredMapView"),
+	// "MapView":            require("./lib/components/maps/MapView")
+};
+
