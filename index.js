e0fba8b109d48febb9976e44de55683876ca4b3e
Michele Belluco <michele-giorgio.belluco@soprasteria.com>
2016-03-08 14:56:41 +0100

    implement swipe to back

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 
 
 module.exports = {
-	"BaseView" : require("./lib/BaseView"),
-	"PageView" : require("./lib/PageView"),
-	"ImageView": require("./lib/ImageView"),
-	"RateView" : require("./lib/RateView"),
-	"dialogs"  : require("./lib/dialogs"),
-	"carousel" : require("./lib/carousel"),
-	"forms"    : require("./lib/forms"),
-	"listviews": require("./lib/listviews"),
-	"loadings" : require("./lib/loadings")
+	"BaseView"   : require("./lib/BaseView"),
+	"PageView"   : require("./lib/PageView"),
+	"ImageView"  : require("./lib/ImageView"),
+	"RateView"   : require("./lib/RateView"),
+	"navigation" : require('./lib/navigations'),
+	"dialogs"    : require("./lib/dialogs"),
+	"carousel"   : require("./lib/carousel"),
+	"forms"      : require("./lib/forms"),
+	"listviews"  : require("./lib/listviews"),
+	"loadings"   : require("./lib/loadings")
 	// "maps"     : require("./lib/dialogs")
 };;
 
