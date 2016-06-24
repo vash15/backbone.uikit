3ab693de22367adcf88e925e338e0d0090431d9d
vash15 <bellucomichele@gmail.com>
2016-06-24 11:08:08 +0200

    Adding navigation view

--- index.js
+++ index.js
@@ -7,10 +7,17 @@ import * as animate from './lib/utils/animate';
 // View Base
 import BaseView from './lib/BaseView';
 import ImageView from './lib/ImageView';
+import PageView from './lib/PageView';
 
 // List View
 import ListView from './lib/listviews/ListView';
 import ListItemView from './lib/listviews/ListItemView';
+
+// Navigations view
+import NavigationView from './lib/navigations/NavigationView';
+import BarView from './lib/navigations/BarView';
+import TitleBarView from './lib/navigations/TitleBarView';
+import IosBarView from './lib/navigations/IosBarView';
 
 let index = {
 	requestAnimationFrame : requestAnimationFrame,
@@ -18,8 +25,13 @@ let index = {
 	animate               : animate,
 	BaseView              : BaseView,
 	ImageView             : ImageView,
+	PageView              : PageView,
 	ListView              : ListView,
-	ListItemView          : ListItemView
+	ListItemView          : ListItemView,
+	NavigationView        : NavigationView,
+	BarView               : BarView,
+	TitleBarView          : TitleBarView,
+	IosBarView            : IosBarView
 };
 
 
@@ -30,6 +42,11 @@ export {
 	animate               as animate,
 	BaseView              as BaseView,
 	ImageView             as ImageView,
+	PageView              as PageView,
 	ListView              as ListView,
-	ListItemView          as ListItemView
+	ListItemView          as ListItemView,
+	NavigationView        as NavigationView,
+	BarView               as BarView,
+	TitleBarView          as TitleBarView,
+	IosBarView            as IosBarView
 };
