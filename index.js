820886f2aa014d6e2fab9bc0c1d5c9dcad4f1d45
SonoIo <matteo.baggio@gmail.com>
2016-06-24 13:02:21 +0200

    Added State model

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 import * as requestAnimationFrame from './lib/utils/requestAnimationFrame';
 import * as style from './lib/utils/style';
 import * as animate from './lib/utils/animate';
+import State from './lib/utils/State';
 
 // View Base
 import BaseView from './lib/BaseView';
@@ -16,6 +17,7 @@ let index = {
 	requestAnimationFrame : requestAnimationFrame,
 	style                 : style,
 	animate               : animate,
+	State:                : State,
 	BaseView              : BaseView,
 	ImageView             : ImageView,
 	ListView              : ListView,
@@ -28,6 +30,7 @@ export {
 	requestAnimationFrame as requestAnimationFrame,
 	style                 as style,
 	animate               as animate,
+	State                 as State,
 	BaseView              as BaseView,
 	ImageView             as ImageView,
 	ListView              as ListView,
