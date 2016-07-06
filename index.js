fc0e2ce654e4e580ffaa53163742ed0771fbcaf6
SonoIo <matteo.baggio@gmail.com>
2016-07-06 10:25:03 +0200

    Move requestNextAnimationFrame

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 // import DetectSwipe from "jquery-detect-swipe";
 
 // Utils
-import * as requestAnimationFrame from './lib/utils/requestAnimationFrame';
+import { requestAnimationFrame, requestNextAnimationFrame } from './lib/utils/requestAnimationFrame';
 import * as style from './lib/utils/style';
 import * as animate from './lib/utils/animate';
 import State from './lib/utils/State';
@@ -38,53 +38,55 @@ import SelectFieldView from './lib/forms/SelectFieldView';
 import CreditCardField from './lib/forms/CreditCardField';
 
 let index = {
-	requestAnimationFrame : requestAnimationFrame,
-	style                 : style,
-	animate               : animate,
-	State                 : State,
-	BaseView              : BaseView,
-	ImageView             : ImageView,
-	PageView              : PageView,
-	RateView              : RateView,
-	ListView              : ListView,
-	ListItemView          : ListItemView,
-	NavigationView        : NavigationView,
-	BarView               : BarView,
-	TitleBarView          : TitleBarView,
-	IosBarView            : IosBarView,
-	ModalView             : ModalView,
-	ImagesModalView       : ImagesModalView,
-	FormView              : FormView,
-	TextField             : TextField,
-	SearchFilterView      : SearchFilterView,
-	SwitchView            : SwitchView,
-	SelectFieldView       : SelectFieldView,
-	CreditCardField       : CreditCardField
+	requestAnimationFrame     : requestAnimationFrame,
+	requestNextAnimationFrame : requestNextAnimationFrame,
+	style                     : style,
+	animate                   : animate,
+	State                     : State,
+	BaseView                  : BaseView,
+	ImageView                 : ImageView,
+	PageView                  : PageView,
+	RateView                  : RateView,
+	ListView                  : ListView,
+	ListItemView              : ListItemView,
+	NavigationView            : NavigationView,
+	BarView                   : BarView,
+	TitleBarView              : TitleBarView,
+	IosBarView                : IosBarView,
+	ModalView                 : ModalView,
+	ImagesModalView           : ImagesModalView,
+	FormView                  : FormView,
+	TextField                 : TextField,
+	SearchFilterView          : SearchFilterView,
+	SwitchView                : SwitchView,
+	SelectFieldView           : SelectFieldView,
+	CreditCardField           : CreditCardField
 };
 
 
 export default index;
 export {
-	requestAnimationFrame as requestAnimationFrame,
-	style                 as style,
-	animate               as animate,
-	State                 as State,
-	BaseView              as BaseView,
-	ImageView             as ImageView,
-	PageView              as PageView,
-	RateView              as RateView,
-	ListView              as ListView,
-	ListItemView          as ListItemView,
-	NavigationView        as NavigationView,
-	BarView               as BarView,
-	TitleBarView          as TitleBarView,
-	IosBarView            as IosBarView,
-	ModalView             as ModalView,
-	ImagesModalView       as ImagesModalView,
-	FormView              as FormView,
-	TextField             as TextField,
-	SearchFilterView      as SearchFilterView,
-	SwitchView            as SwitchView,
-	SelectFieldView       as SelectFieldView,
-	CreditCardField       as CreditCardField
+	requestAnimationFrame     as requestAnimationFrame,
+	requestNextAnimationFrame as requestNextAnimationFrame,
+	style                     as style,
+	animate                   as animate,
+	State                     as State,
+	BaseView                  as BaseView,
+	ImageView                 as ImageView,
+	PageView                  as PageView,
+	RateView                  as RateView,
+	ListView                  as ListView,
+	ListItemView              as ListItemView,
+	NavigationView            as NavigationView,
+	BarView                   as BarView,
+	TitleBarView              as TitleBarView,
+	IosBarView                as IosBarView,
+	ModalView                 as ModalView,
+	ImagesModalView           as ImagesModalView,
+	FormView                  as FormView,
+	TextField                 as TextField,
+	SearchFilterView          as SearchFilterView,
+	SwitchView                as SwitchView,
+	SelectFieldView           as SelectFieldView,
+	CreditCardField           as CreditCardField
 };
